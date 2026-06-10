8c3afc8017d8f5c1940990a17c693ba283252d7a
skryce <141428413+novacore360@users.noreply.github.com>
2026-06-10 10:22:58 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
